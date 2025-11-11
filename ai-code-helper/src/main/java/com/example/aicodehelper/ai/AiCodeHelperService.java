2d48f5e3aff28e8e4f31e6858a5528003ef8ea6d
zhuangwang1029 <1905212870@qq.com>
2025-11-11 22:15:50 +0800

    删除无关引用

--- ai-code-helper/src/main/java/com/example/aicodehelper/ai/AiCodeHelperService.java
+++ ai-code-helper/src/main/java/com/example/aicodehelper/ai/AiCodeHelperService.java
@@ -1,16 +1,13 @@
 package com.example.aicodehelper.ai;
 
-import com.example.aicodehelper.ai.guardrail.SafeInputGuardrail;
 import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.Result;
 import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
-import dev.langchain4j.service.guardrail.InputGuardrails;
 import reactor.core.publisher.Flux;
 
 import java.util.List;
 
-@InputGuardrails({ SafeInputGuardrail.class })
 public interface AiCodeHelperService {
     @SystemMessage(fromResource = "system-prompt.txt")
     String chat(String message);
